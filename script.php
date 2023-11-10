5bfd1fced45b0e8bde4e38873becf3fbbcd32330
tthkra <142266646+tthkra@users.noreply.github.com>
2023-11-10 12:53:20 +0300

    Update script.php

--- script.php
+++ script.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "directiondb";
+$dbname = "robot";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,22 +16,22 @@ if ($conn->connect_error) {
     echo "Connection successful<br>";
 }
 
-// Insert values into the db
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $direction = $_POST['directions'];
-    $distance = $_POST['distance'];
-            
-    $query = "INSERT INTO moves (direction, distance_m) VALUES 
+$data = json_decode(file_get_contents("php://input"), true);
+
+$direction = $data["direction"];
+$distance = $data["distance"];
+        
+$query = "INSERT INTO moves (direction, distance_m) VALUES 
     ('$direction', '$distance')";
 
-    if ($conn->query($query)) {
-        echo "value inserted into database";
-    } 
-    else {
-        echo "error: " . $conn->error;
-    }
+if ($conn->query($query)) {
+    echo "value inserted into database";
+} 
+else {
+    echo "error: " . $conn->error;
 }
+
 
 
 $conn->close();
