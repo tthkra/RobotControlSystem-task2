922f103ad35ced8730569eeea5234cd5777ed420
tthkra <142266646+tthkra@users.noreply.github.com>
2023-11-10 12:56:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-fetch('http://localhost/retrieve.php')
-  .then(response => response.json())
-  .then(data => {
-    const direction = data.direction;
-    const distance = data.distance * 10;
+var lastEndpoint = { x: 0, y: 0 };
 
-    draw(direction, distance);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+document.querySelector('form').addEventListener('submit', function(event) {
 
-  
+  const direction = document.querySelector('select[name="directions"]').value;
+  var distance = document.querySelector('input[name="distance"]').value;
+
+  sendData(direction, distance);
+
+  draw(direction, distance);
+});
+
 function isCanvasBlank(canvas) {
   return (
     !canvas.getContext('2d')
       .getImageData(0, 0, canvas.width, canvas.height)
       .data.some(channel => channel !== 0)
   );
-};
+}
 
-function draw( direction, distance) {
-
+function draw(direction, distance) {
   const canvas = document.querySelector('#canvas');
   if (!canvas.getContext) {
     return;
@@ -34,27 +32,42 @@ function draw( direction, distance) {
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 2;
 
-  const lastX = parseInt(localStorage.getItem('lastX')) || 0;
-  const lastY = parseInt(localStorage.getItem('lastY')) || 0;
+  const lastX = lastEndpoint.x;
+  const lastY = lastEndpoint.y;
+
+  // represent every one meter distance with 10 pixels, just for visual clarification
+  distance = distance * 10;
 
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
 
   if (direction === 'forward') {
     ctx.lineTo(lastX, lastY - distance);
-    localStorage.setItem('lastY', lastY - distance);
+    lastEndpoint = { x: lastX, y: lastY - distance };
   } else if (direction === 'left') {
     ctx.lineTo(lastX - distance, lastY);
-    localStorage.setItem('lastX', lastX - distance);
+    lastEndpoint = { x: lastX - distance, y: lastY };
   } else if (direction === 'right') {
     ctx.lineTo(lastX + distance, lastY);
-    localStorage.setItem('lastX', lastX + distance);
+    lastEndpoint = { x: lastX + distance, y: lastY };
   } else if (direction === 'backward') {
     ctx.lineTo(lastX, lastY + distance);
-    localStorage.setItem('lastY', lastY + distance);
+    lastEndpoint = { x: lastX, y: lastY + distance };
   }
 
   ctx.stroke();
+}
 
-};
+function sendData(direction, distance) {
+  var data = {
+      direction : direction,
+      distance : distance
+  };
 
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "script.php", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.send(JSON.stringify(data));
+}
